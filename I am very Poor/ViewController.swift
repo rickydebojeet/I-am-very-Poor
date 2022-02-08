0dba7d1659cf7af5a677bd045299af76ac3118e3
Debojeet Das <43584277+rickydebojeet@users.noreply.github.com>
2022-02-08 18:55:20 +0530

    Initial Commit

---
+++ I am very Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am very Poor
+//
+//  Created by Debojeet Das on 08/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
